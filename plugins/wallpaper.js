4aab5f7204835e603592bafabc2f4dbc5a890719
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-24 23:54:41 +0530

    Create wallpaper.js

---
+++ plugins/wallpaper.js
@@ -0,0 +1,30 @@
+const { wallpaper } = require('../lib/scraper');
+async function execute(bosco, msg, match) {
+if (!match) return reply(`Example : ${handlers}wallpaper gojo`)
+anu = await wallpaper(match)
+   result = anu[Math.floor(Math.random() * anu.length)]
+let buttons = [
+  {buttonId: `${handlers}wallpaper ${match}`, buttonText: {displayText: 'Next'}, type: 1}
+   ]
+let pepe = ''
+pepe += '_Title_        : *' + result.title + '*\n\n';
+pepe += '_Category_     : *' + result.type + '*\n\n';
+peep += '_Detail_       : *' + result.source + '*\n\n';
+pepe += '_Media url_    : *' + result.image[2] || result.image[1] || result.image[0] + '*\n\n';
+  let buttonMessage = {
+  image: { url: result.image[0] },
+  caption: pepe,
+  footer: bot_footer,
+  buttons: buttons,
+  headerType: 4
+   }
+   bosco.sendMessage(msg.key.remoteJid, buttonMessage, { quoted: msg })
+}
+
+module.exports = { 
+    command: "wallpaper", 
+    Type: "download", 
+    isDependent: false, 
+    desc: "used to download wallpapers.", 
+    execute,
+};
