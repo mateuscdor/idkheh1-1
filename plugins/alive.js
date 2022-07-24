0ef5fba9dc64f6cba02e1ceb522850aae5de90f2
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-24 16:23:43 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const {getBuffer} = require("../lib/myfunc");
-async function execute(bosco, msg) {
+async function execute(bosco,msg) {
 let alivevid = await getBuffer(alive_vid)
     const buttons = [
         { urlButton: { displayText: `Instagram`, url: 'https://instagram.com/pepe.sir_?' } },
         { quickReplyButton: { displayText: `Menu`, id: `.menu` } }
     ];
-    await bosco.send5ButGif(from, alive_text, bot_footer, alivevid, buttons);
+    await bosco.send5ButGif(msg.key.remoteJid, alive_text, bot_footer, alivevid, buttons);
 }
 
 module.exports = { 
