03f3869b50215de9b1ea95bec67bcb73bfff990b
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-25 00:18:32 +0530

    Create toaudio.js

---
+++ plugins/toaudio.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const {toAudio} = require("../lib/converter");
+async function execute(bosco, msg, match) {
+var quoted = msg.quoted ? msg.quoted : msg
+if (isQuotedVideo || isQuotedAudio) {
+let media = await bosco.downloadAndSaveMediaMessage(quoted);
+ let audio = await toAudio(media, 'mp4')
+ bosco.sendMessage(msg.key.remoteJid, {audio: audio, mimetype: 'audio/mpeg'}, { quoted : msg })
+} else {
+reply(_Reply to video/audio_)
+}
+}
+
+module.exports = { 
+    command: "toaudio", 
+    Type: "convert", 
+    isDependent: false, 
+    desc: "used to convert video/audio to audio", 
+    execute,
+};
