a26a2bf5f5993281a7763b1473b291f1551097ca
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-24 17:43:17 +0530

    Update qr.js

--- plugins/qr.js
+++ plugins/qr.js
@@ -10,6 +10,6 @@ async function qrgen(text) {
 async function execute(bosco, msg, match) {
     if (!match) return reply(`Example : ${handlers}qr bosco`)
     let data = await qrgen(match);
-    await bosco.sendMessage(from, { image: data.data, contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply: { title: bot_name, body: bot_footer, previewType: "PHOTO", thumbnail: bot_img, sourceUrl: owner_link } }, caption: `QR code for 👇\n` + "```" + match + "```" }, { quoted: msg });
+    await bosco.sendMessage(msg.key.remoteJid, { image: data.data, contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply: { title: bot_name, body: bot_footer, previewType: "PHOTO", thumbnail: bot_img, sourceUrl: owner_link } }, caption: `QR code for 👇\n` + "```" + match + "```" }, { quoted: msg });
 }
 module.exports = { command: "qr", Type: "tools", isDependent: false, desc: "used to generate qr for given text", execute,};
