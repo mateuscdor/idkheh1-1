ef5cfe32d2d7c76e8ba397fc2151a7774a7f5427
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-24 22:29:19 +0530

    Update mode.js

--- plugins/mode.js
+++ plugins/mode.js
@@ -1,14 +1,34 @@
 const fs = require("fs");
 const {getBuffer} = require("../lib/myfunc");
 async function execute(bosco, msg, match) {
-if (bosco.public === true){
+
+  if (!msg.key.fromMe) return reply('_only for owner_')
+  if (bosco.public === true){
 mode = 'public'
 } else {
 mode = 'private'
 }
+  if (!match) {
 var but = [{buttonId: `${handlers}mode public`, buttonText: { displayText: 'public' }, type: 1 },
 {buttonId: `${handlers}mode private`, buttonText: { displayText: 'private' }, type: 1 }]
 bosco.sendMessage(from, { caption: `working mode configuration `, image: { url: botimg }, buttons: but, footer: `Current mode : ${mode}` }, { quoted: msg })
+  }
+  let Type = args.shift();
+    switch (Type) {
+        case "private": case "self": {
+
+bosco.public = false
+   reply('_Private mode enabled ✅_')
+
+    }
+        break
+   case "public": {
+
+bosco.public = true
+   reply('_Public mode enabled ✅_')
+    }
+        break
+    }
 }
 
 module.exports = { 
