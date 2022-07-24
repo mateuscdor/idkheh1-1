db6560cae74138a58d45d347d8ed435476aa71c1
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-25 04:17:44 +0530

    Create quote.js

---
+++ plugins/quote.js
@@ -0,0 +1,16 @@
+const {fetchJson} = require("../lib/myfunc");
+const {getRandom} = require('../lib/functions');
+async function execute(bosco, msg) {
+    var data = await fetchJson(`https://gist.githubusercontent.com/pepesir/6209507f9caf191ab20c154d6de53bf1/raw/a97d9d9ca32c3084b0ddd407cf1aed89eccb214b/quotes.json`);
+    var hehe = data.result;
+    var quote = hehe[Math.floor(Math.random() * hehe.length)];
+    reply(quote.quote + '\n\n-- ' + quote.author);
+}
+
+module.exports = {
+  command: "quote",
+  Type: "tools", 
+  isDependent: false, 
+  desc: "used to get random quotes", 
+  execute,
+};
