2733dceab3a0d868f39edfa04739572c1f064b27
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-24 22:37:12 +0530

    Create trt.js

---
+++ plugins/trt.js
@@ -0,0 +1,19 @@
+const translate = require('@vitalets/google-translate-api');
+async function execute(bosco, msg) {
+    if (!match)
+        return reply(`_Example : ${handlers}trt {ml} text_`);
+    let
+        LANG = 'en', trtMessage = match;
+    if (langMatch = lang.match("\\{([a-z]{2})\\}")) {
+        LANG = langMatch[1];
+        trtMessage = trtMessage.replace(langMatch[0], "");
+    }
+    translate(trtMessage, {
+        to: LANG
+    }).then(async (res) => {
+        if ("text" in res) {
+            await reply(res.text);
+        }
+    });
+}
+module.exports = {command: "trt", Type: "tools", isDependent: false, desc: "used to translate", execute,};
