437a62817a4dc4842556db630d8ccab3582e5067
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-25 06:02:47 +0530

    Update stop.js

--- plugins/stop.js
+++ plugins/stop.js
@@ -1,17 +1,16 @@
 const FilterDb = require('./sql/filters');
-let FILTERED = "*✅ Successfully set* ```{}``` *to filter!*"
 async function execute(bosco, msg, match) {
 match = match.match(/[\'\"\“](.*?)[\'\"\“]/gsm);
     if (match === null) {
-        return await reply("*❌ Please type in reply!*\n*Example:*" + '\n*Example:* ```.stop "hello"```')
+        return await reply(`_Example : ${handlers}stop "hii"_`)
     }
 
     del = await FilterDb.deleteFilter(msg.key.remoteJid, match[0].replace(/['"“]+/g, ''));
     
     if (!del) {
-        await reply("*❌ There is already no filter like this!*")
+        await reply("_❌ There is already no filter like this!_")
     } else {
-        await reply("*✅ The filter was successfully deleted!*")
+        await reply("_✅ The filter was successfully deleted!_")
     }
 
 
@@ -20,6 +19,6 @@ module.exports = {
     command: "stop", 
     Type: "group", 
     isDependent: false, 
-    desc: "Used to stop previous added filter.", 
+    desc: "Used to stop filter.", 
     execute,
 };
