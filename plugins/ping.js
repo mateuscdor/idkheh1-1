cc22a924288b8417addc9350c34fc14dd4e66182
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-26 07:41:37 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,14 +1,61 @@
-const performance = require('perf_hooks');
+const { performance } = require('perf_hooks')
+let osu = require('node-os-utils')
+async function execute(bosco, msg, match) {
+    try {
+        let NotDetect = 'Not Detect'
+        let old = performance.now()
+        let cpu = osu.cpu
+        let cpuCore = cpu.count()
+        let drive = osu.drive
+        let mem = osu.mem
+        let netstat = osu.netstat
+        let OS = osu.os.platform()
+        let cpuModel = cpu.model()
+        let cpuPer
+        let p1 = cpu.usage().then(cpuPercentage => {
+            var cpuPer = cpuPercentage
+        }).catch(() => {
+            var cpuPer = NotDetect
+        })
+        let driveTotal, driveUsed, drivePer
+        let p2 = drive.info().then(info => {
+            driveTotal = (info.totalGb + ' GB'),
+                driveUsed = info.usedGb,
+                drivePer = (info.usedPercentage + '%')
+        }).catch(() => {
+            driveTotal = NotDetect,
+                driveUsed = NotDetect,
+                drivePer = NotDetect
+        })
+        let ramTotal, ramUsed
+        let p3 = mem.info().then(info => {
+            ramTotal = info.totalMemMb,
+                ramUsed = info.usedMemMb
+        }).catch(() => {
+            ramTotal = NotDetect,
+                ramUsed = NotDetect
+        })
+        let netsIn, netsOut
+        let p4 = netstat.inOut().then(info => {
+            netsIn = (info.total.inputMb + ' MB'),
+                netsOut = (info.total.outputMb + ' MB')
+        }).catch(() => {
+            netsIn = NotDetect,
+                netsOut = NotDetect
+        })
+        await Promise.all([p1, p2, p3, p4])
+        await reply(`_Testing status..._`)
+        let _ramTotal = (ramTotal + ' MB')
+        let neww = performance.now()
+        let status =`_Ping_ : ${Math.round(neww - old)} ms
+`
 
-async function execute(bosco, msg) {
-try {
-var heh = performance.now()
-var hehe = performance.now()
-reply(`_Ping_ : ${Math.round(heh - hehe)} ms`)
+bosco.sendMessage(msg.key.remoteJid, {text :status},{quoted : msg})
 } catch (err) {
 reply(err)
 }
 }
+
 
 module.exports = { 
     command: "ping", 
