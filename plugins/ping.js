4c2c35ba3f364aaead39511a61de472ac704c574
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-26 06:39:19 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,68 +1,13 @@
-const speed = require("performance-now");
 const performance = require('perf_hooks');
-const os = require('os');
-const formatp = require("../lib/myfunc");
-const runtime = function (seconds) {
-seconds = Number(seconds);
-var d = Math.floor(seconds / (3600 * 24));
-var h = Math.floor((seconds % (3600 * 24)) / 3600);
-var m = Math.floor((seconds % 3600) / 60);
-var s = Math.floor(seconds % 60);
-var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " Day, ") : "";
-var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " Hour, ") : "";
-var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " Minute, ") : "";
-var sDisplay = s > 0 ? s + (s == 1 ? " second" : " Second") : "";
-return dDisplay + hDisplay + mDisplay + sDisplay;
-}
+
 async function execute(bosco, msg, match) {
 try {
-let timestamp = speed()
-let latensi = speed() - timestamp
-let neww = performance.now()
-let oldd = performance.now()
-// Bot Status
-const used = process.memoryUsage()
-const cpus = os.cpus().map(cpu => {
-cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-return cpu
- })
-const cpu = cpus.reduce((last, cpu, _, { length }) => {
-last.total += cpu.total
-last.speed += cpu.speed / length
-last.times.user += cpu.times.user
-last.times.nice += cpu.times.nice
-last.times.sys += cpu.times.sys
-last.times.idle += cpu.times.idle
-last.times.irq += cpu.times.irq
-return last
-}, {
-speed: 0,
-total: 0,
-times: {
-user: 0,
-nice: 0,
-sys: 0,
-idle: 0,
-irq: 0
-}
-})
-
-let respond = `
-Response Speed ${latensi.toFixed(4)} _Second_ \n ${oldd - neww} _miliseconds_\n\nRuntime : ${runtime(process.uptime())}
- Info Server
-RAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
-_NodeJS Memory Usaage_
-${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n')}
-${cpus[0] ? `_Total CPU Usage_
-${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
-_CPU Core(s) Usage (${cpus.length} Core CPU)_
-${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
-`.trim()
-reply(respond)
+let new = performance.now()
+let old = performance.now()
+reply(`_Ping_ : ${Math.round(new - old)} ms`)
 } catch (err) {
 reply(err)
 }
-
 }
 
 module.exports = { 
