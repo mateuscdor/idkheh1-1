c0433bc372af173c39cce81d688fd1dba7167414
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-27 05:58:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,44 @@ const config = require('./config');
 const { getMessage } = require("./plugins/sql/greetings");
 const { exec, spawn } = require("child_process");
 const store = makeInMemoryStore({ logger: logg().child({ level: 'silent', stream: 'store' }) })
+// Keep alive
+const http = require('http');
+const Agent = require('agentkeepalive');
+
+const keepaliveAgent = new Agent({
+  maxSockets: 100,
+  maxFreeSockets: 10,
+  timeout: 60000, // active socket keepalive for 60 seconds
+  freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
+});
+
+const options = {
+  host: 'cnodejs.org',
+  port: 80,
+  path: '/',
+  method: 'GET',
+  agent: keepaliveAgent,
+};
+
+const req = http.request(options, res => {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+req.on('error', e => {
+  console.log('problem with request: ' + e.message);
+});
+req.end();
+
+setTimeout(() => {
+  if (keepaliveAgent.statusChanged) {
+    console.log('[%s] agent status changed: %j', Date(), keepaliveAgent.getCurrentStatus());
+  }
+}, 2000);
+
 const boscoDB = config.DATABASE.define('bosco', {
     info: {
       type: DataTypes.STRING,
