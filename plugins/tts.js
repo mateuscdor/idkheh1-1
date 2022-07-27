3d5bceaaeb3049dbd6244da17d75a5d6974f7a9b
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-27 06:41:51 +0530

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -1,7 +1,7 @@
 const googleTTS = require('google-translate-tts');
 async function execute(bosco, msg, match) {
     if (!match)
-        return await reply(`_Example : ${handlers}tts {ml} hello_`);
+        return await bosco.reply(msg.key.remoteJid,`_Example : ${handlers}tts {ml} hello_`);
     let
         LANG = 'en', ttsMessage = match, SPEED = 1.0;
     if (langMatch = match.match("\\{([a-z]{2})\\}")) {
@@ -18,7 +18,7 @@ async function execute(bosco, msg, match) {
             voice: LANG
         });
     } catch {
-        return await reply("_Error ;)_");
+        return await bosco.reply(msg.key.remoteJid,"_Error ;)_");
     }
     await bosco.sendMessage(msg.key.remoteJid, { audio: buffer, mimetype: 'audio/mp4', duration: 359996400, ptt: false, contextInfo: { forwardingScore: 9999, externalAdReply: { title: bot_footer, body: '', previewType: "PHOTO", thumbnail: bot_img, sourceUrl: owner_link } }, sendEphemeral: true }, { quoted: msg });
 
