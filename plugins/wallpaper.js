ecf764d22f686ba86d108434443bbe0f1b01e607
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-30 06:08:04 +0530

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1,15 +1,16 @@
 const { wallpaper } = require('../lib/scraper');
 async function execute(bosco, msg, match) {
-if (!match) return bosco.reply(msg.key.remoteJid,`Example : ${handlers}wallpaper gojo`)
+if (!match) return reply(`Example : ${handlers}wallpaper gojo`)
 anu = await wallpaper(match)
    result = anu[Math.floor(Math.random() * anu.length)]
 let buttons = [
   {buttonId: `${handlers}wallpaper ${match}`, buttonText: {displayText: 'Next'}, type: 1}
    ]
 let pepe = ''
-pepe += '_Title_        : *' + result.title + '*\n\n';
-pepe += '_Category_     : *' + result.type + '*\n\n';pepe += '_Detail_       : *' + result.source + '*\n\n';
-pepe += '_Media url_    : *' + result.image[2] || result.image[1] || result.image[0] + '*';
+pepe += '⬘ _Title_        : *' + result.title + '*\n\n';
+pepe += '⬘ _Category_     : *' + result.type + '*\n\n';
+pepe += '⬘ _Detail_       : *' + result.source + '*\n\n';
+pepe += '⬘ _Media url_    : *' + result.image[2] || result.image[1] || result.image[0] + '*';
   let buttonMessage = {
   image: { url: result.image[0] },
   caption: pepe,
