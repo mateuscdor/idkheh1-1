f21da2f041bbd7948c2e71883ccddcac4d3b29f6
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-30 15:45:48 +0530

    Create status.js

---
+++ plugins/status.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const {getBuffer, fetchJson, isUrl} = require("../lib/myfunc");
+async function execute(bosco, msg, match) {
+console.log(match)
+if (isUrl(match)) {
+              for(let i of match) {		
+		        if(i.includes('mp4')){		           			    				
+				let link = await getBuffer(i)
+                bosco.sendMessage(msg.key.remoteJid, { video: link, jpegThumbnail: bot_img }, { quoted: msg })
+                } else {
+                let link = await getBuffer(i)
+                bosco.sendMessage(msg.key.remoteJid, { image: link, jpegThumbnail: bot_img }, { quoted: msg })                      
+               }
+              }
+} else {
+let efxv = await fetchJson('https://api.pepesir.repl.co/efxvid.json')
+let number = 1
+let sections = []  
+for (let hi of efxv.result) {
+const list = {title: number++,
+      rows: [
+         {
+          title: `${hi.caption}`,
+          description: `${hi.desc}`,
+          rowId: `${handlers}status ${hi.video}`
+}, 
+]
+}
+}
+sections.push(list)   
+  await bosco.sendMessage(
+msg.key.remoteJid, 
+{
+text: "\n\n",
+footer: bot_footer,
+title: `Video list`,
+buttonText: "CLICK HERE",
+sections
+}, { quoted : msg }) 
+}
+}
+
+module.exports = { 
+    command: "status", 
+    Type: "download", 
+    isDependent: false, 
+    desc: "", 
+    execute,
+};
