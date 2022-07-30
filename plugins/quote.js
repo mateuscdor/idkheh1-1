c05178cbfd797f6757574da5fce54eb025b240ad
idkheh123 <109895975+idkheh123@users.noreply.github.com>
2022-07-30 06:20:41 +0530

    Update quote.js

--- plugins/quote.js
+++ plugins/quote.js
@@ -4,7 +4,7 @@ async function execute(bosco, msg) {
     var data = await fetchJson(`https://gist.githubusercontent.com/pepesir/6209507f9caf191ab20c154d6de53bf1/raw/a97d9d9ca32c3084b0ddd407cf1aed89eccb214b/quotes.json`);
     var hehe = data.result;
     var quote = hehe[Math.floor(Math.random() * hehe.length)];
-    await bosco.reply(msg.key.remoteJid,quote.quote + '\n\n-- ' + quote.author);
+    await reply(quote.quote + '\n\n-- ' + quote.author);
 }
 
 module.exports = {
